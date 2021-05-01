08c622d533a773a4779c196f4ff1782a113ef50a
harvin pratama putra <harvin.pratama@gmail.com>
2021-05-01 12:33:10 +0700

    test

--- src/components/SortingController/SortingController.js
+++ src/components/SortingController/SortingController.js
@@ -7,11 +7,11 @@ function SortingController(props) {
     return (
         <div>
             <div className={ styles.Controller }>
-                <Button redirect={ "." } effect={ props.effect } name="Create New Array" />
-                <Button redirect={ "." } effect={ props.bubbleSort } name="Bubble Sort" />
-                <Button redirect={ "." } effect={ props.selectionSort } name="Selection Sort" />
-                <Button redirect={ "." } effect={ props.mergeSort } name="Merge Sort" />
-                <Button redirect={ "." } effect={ props.quickSort } name="Quick Sort" />
+                <Button redirect={ "/sorting" } effect={ props.effect } name="Create New Array" />
+                <Button redirect={ "/sorting" } effect={ props.bubbleSort } name="Bubble Sort" />
+                <Button redirect={ "/sorting" } effect={ props.selectionSort } name="Selection Sort" />
+                <Button redirect={ "/sorting" } effect={ props.mergeSort } name="Merge Sort" />
+                <Button redirect={ "/sorting" } effect={ props.quickSort } name="Quick Sort" />
                 <Input type="range" value={ props.timeValue } label="Time" />
                 <Input type="range" value={ props.barValue } label="Bar" />
             </div>
